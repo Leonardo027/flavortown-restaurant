7f28506428a7dd017b123dc44c0daf07df730879
Roberto <cantelleleonardo@gmail.com>
2026-01-22 06:52:48 -0300

    fix: correcao config nextjs 15

--- flavortown-restaurant/next.config.ts
+++ flavortown-restaurant/next.config.ts
@@ -1,11 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  typescript: {
+import type { NextConfig } from "next";
 
+const nextConfig: NextConfig = {
+  typescript: {
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
 };
 
